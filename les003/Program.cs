1348ebeee2f1457da8b35c240ea5d3a2d224866c
max <maczejko.max@ya.ru>
2022-07-12 19:56:27 +0600

    kod1

---
+++ les003/Program.cs
@@ -0,0 +1,6 @@
+﻿//  код который принимает на вход три числа ина выходе показывает последнюю цифру этого числа
+Console.WriteLine("Введите трехзначное число");
+int NumA = Convert.ToInt32(Console.ReadLine());
+string Number = Convert.ToString(NumA);
+Console.WriteLine("последняя цифра этого числа :" + Number[2]);
+
